d10011dc7c78bdc8a56d8cc227e29e3b9f9bc3c0
pond-e <haruto2266@icloud.com>
2022-10-20 15:57:18 +0900

    fix while loop

--- remembering_remote_controller.php
+++ remembering_remote_controller.php
@@ -7,11 +7,13 @@
 </head>
 <body>
 
-<h1>記憶完了</h1>
-
-<div class="page_change">
-    <a href="remember_remote_controller.php" class="btn btn--orange">ボタン選択画面に戻る</a>
+<h1>記憶フェーズ</h1>
+<div class="remote_controller_button">
+    <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+    <input type=submit class="btn btn--yellow btn--cubic" value="記憶が完了したか確認する" name="conform">
+    </form>
 </div>
+
 
 <?php
     $buffer = "none";
@@ -41,31 +43,6 @@
         fclose($fp);
         $data = "none";
 
-
-        //記憶が完了するまで待つ
-        while($buffer !== "success"){
-            $Raspi_receive_file = 'Raspi_receive.txt';
-
-            $fp_read = fopen($Raspi_receive_file, 'rb');
-    
-            if ($fp_read){
-                //print('2<br>');
-                if (flock($fp_read, LOCK_SH)){
-                    //print('3<br>');
-                    while (!feof($fp_read)) {
-                        $buffer = fgets($fp_read);
-                        //print($buffer.'<br>');
-                    }
-                    //print('4<br>');
-                    flock($fp_read, LOCK_UN);
-                }else{
-                    print('ファイルロックに失敗しました');
-                }
-            }
-            fclose($fp_read);
-        }
-
-
         // 記憶終了
         $Raspi_state_file = 'Raspi_state.txt';
         $fp = fopen($Raspi_state_file, 'wb');
@@ -82,31 +59,49 @@
             print('file open error');
         }
 
-        $Raspi_state_file = 'Raspi_receive.txt';
-        $fp = fopen($Raspi_state_file, 'wb');
-        if ($fp){
-            if (flock($fp, LOCK_EX)){
-                if (fwrite($fp, "none") === FALSE){
-                    print('ファイル書き込みに失敗しました');
-                }
-                flock($fp, LOCK_UN);
-            }else{  
-                print('ファイルロックに失敗しました');
-            }
-        }else{
-            print('file open error');
-        }
+        
     }
 
-    if($buffer === "success"){
-        //echo "success!!";
-    }else{
-        //echo $buffer;
+    if(isset($_POST['conform'])){
+        $Raspi_receive_file = 'Raspi_receive.txt';
+
+        $fp_read = fopen($Raspi_receive_file, 'rb');    
+        if ($fp_read){
+            //print('2<br>');
+            if (flock($fp_read, LOCK_SH)){
+                //print('3<br>');
+                while (!feof($fp_read)) {
+                    $buffer = fgets($fp_read);
+                    //print($buffer.'<br>');
+                }
+                //print('4<br>');
+                flock($fp_read, LOCK_UN);
+            }else{
+                print('ファイルロックに失敗しました');
+            }
+        }
+        fclose($fp_read);
+
+        if($buffer === "success"){
+            print('<div class="page_change">
+                    <a href="remember_remote_controller.php" class="btn btn--orange">ボタン選択画面に戻る</a>
+                    </div>');
+            $Raspi_state_file = 'Raspi_receive.txt';
+            $fp = fopen($Raspi_state_file, 'wb');
+            if ($fp){
+                if (flock($fp, LOCK_EX)){
+                    if (fwrite($fp, "none") === FALSE){
+                        print('ファイル書き込みに失敗しました');
+                    }
+                    flock($fp, LOCK_UN);
+                }else{  
+                    print('ファイルロックに失敗しました');
+                }
+            }else{
+                print('file open error');
+            }
+        }
     }
-    //print_r($buffer);
-    //print('<br>');
-    //print(gettype($buffer));
-    //
 
 ?>
 
