07db6a8c4b1a342da9e390651e3fef7722429bc5
pond-e <haruto2266@icloud.com>
2022-11-22 19:27:08 +0900

    address login page for smart phone

--- login.php
+++ login.php
@@ -40,6 +40,7 @@ header('Content-Type: text/html; charset=UTF-8');
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="styles/login.css" rel="stylesheet">
     <title>Temペット Login</title>
 </head>
